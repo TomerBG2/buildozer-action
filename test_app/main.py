232529145a24814005f0b03da19a6cfb0f2d35c8
Tomer Ben Gera <bengeratomer@gmail.com>
2023-07-21 18:23:11 +0300

    Main screen added to choose between two tours options
    Signed-off-by: Tomer Ben Gera <bengeratomer@gmail.com>

--- test_app/main.py
+++ test_app/main.py
@@ -1,27 +1,46 @@
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivymd.app import MDApp
+from kivymd.app import MDApp, App
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class SalaryCalcLayout(BoxLayout):
+class SunsetTour(Screen):
+    def calculate(self, instance):
+        try:
+            tip_amount = int(self.ids.tip_amount.text)
+            base_salary = int(self.ids.base_salary.text)
+            result = ((tip_amount - base_salary) * 0.3) + base_salary
+            self.ids.result_label.text = f"Your salary is: {result}, company gets {tip_amount - result}"
+        except ValueError:
+            self.ids.result_label.text = "Please enter integers"
+
+
+class MenuScreen(Screen):
     pass
+
+class IntroTour(Screen):
+    def calculate(self, instance):
+        try:
+            tip_amount = int(self.ids.tip_amount.text)
+            base_salary = int(self.ids.base_salary.text)
+            result = (tip_amount - base_salary) * 0.25 + base_salary
+            self.ids.result_label.text = f"Your salary is: {result}, company gets {tip_amount - result}"
+        except ValueError:
+            self.ids.result_label.text = "Please enter integers"
 
 
 class SalaryCalc(MDApp):
-    def calculate(self, instance):
-        try:
-            tip_amount = int(self.root.ids.tip_amount.text)
-            base_salary = int(self.root.ids.base_salary.text)
-            result = (tip_amount - base_salary) * 0.25 + base_salary
-            self.root.ids.result_label.text = f"Your salary is: {result}, company gets {tip_amount - result}"
-        except ValueError:
-            self.root.ids.result_label.text = "Please enter integers"
-
     def build(self):
-        self.theme_cls.primary_palette = "DeepPurple"
-        self.theme_cls.primary_hue = "A400"
+        self.theme_cls.primary_palette = "Purple"
+        self.theme_cls.primary_hue = "300"
         Builder.load_file('my2.kv')
-        return SalaryCalcLayout()
+        # # Create the screen manager
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(IntroTour(name='IntroTour'))
+        sm.add_widget(SunsetTour(name='SunsetTour'))
+
+        return sm
         
 
 
